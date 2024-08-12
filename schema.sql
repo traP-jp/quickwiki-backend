c631cf316cf6c4debd024d462f031fa13c047ba4
kavos <kohei.hadano@gmail.com>
2024-08-12 18:08:20 +0900

    setup schema.sql

--- schema.sql
+++ schema.sql
@@ -1,9 +1,20 @@
 CREATE TABLE wikis (
-    id SERIAL PRIMARY KEY,
+    id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name TEXT NOT NULL,
-    description TEXT NOT NULL,
+    type TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    owner_id INTEGER NOT NULL,
-    content TEXT NOT NULL
+    owner_traq_id INTEGER NOT NULL,
+    content TEXT NOT NULL, 
+    tags TEXT[]
+);
+CREATE TABLE lectures (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    content TEXT NOT NULL,
+    parent_id INT(11)
+    KEY parent_id_idx (parent_id)
+        FOREIGN KEY (parent_id)
+        REFERENCES lectures(id)
 );
