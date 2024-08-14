5ca2f8a178a21ef20073e27e7d4cf4d64dc6eae5
kavos <kohei.hadano@gmail.com>
2024-08-14 12:10:19 +0900

    fix db schema

--- schema.sql
+++ schema.sql
@@ -7,23 +7,54 @@ CREATE TABLE wikis (
     owner_traq_id INTEGER NOT NULL,
     content TEXT NOT NULL
 );
-CREATE TABLE lectures (
+CREATE TABLE messages (
     id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    title TEXT NOT NULL,
+    wiki_id INT(11) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    user_traq_id CHAR(36) NOT NULL,
+    channel_id CHAR(36) NOT NULL,
     content TEXT NOT NULL,
-    folder_id INT(11)
-    folder_path TEXT NOT NULL,
+    FOREIGN KEY (wiki_id)
+        REFERENCES wikis(id)
+);
+CREATE TABLE messageStamps (
+    id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    message_id INT(11) NOT NULL,
+    stamp_traq_id CHAR(36) NOT NULL,
+    count INT(11) NOT NULL,
+    FOREIGN KEY (message_id)
+        REFERENCES messages(id)
+);
+CREATE TABLE memos (
+    id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    wiki_id INT(11) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    owner_traq_id INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    FOREIGN KEY (wiki_id)
+        REFERENCES wikis(id)
 );
 CREATE TABLE tags (
     id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name TEXT NOT NULL
 );
 CREATE TABLE tags_in_wiki (
+    wiki_id INT(11) NOT NULL,
+    tag_id INT(11) NOT NULL,
     PRIMARY KEY (wiki_id, tag_id),
     FOREIGN KEY (wiki_id)
         REFERENCES wikis(id),
     FOREIGN KEY (tag_id)
         REFERENCES tags(id)
+);
+CREATE TABLE lectures (
+    id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL,
+    folder_id INT(11)
+    folder_path TEXT NOT NULL,
 );
 CREATE TABLE folders (
     id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
